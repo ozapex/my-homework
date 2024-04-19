a229769299b6d1874c55d9a578d7f2e06ec60c5c
Oscar Eduardo Ruiz <oeduardoruiz@apexsystems.com>
2024-04-19 10:50:37 -0600

    finished

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { FirstJobComponent } from './first-job/first-job.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'firstJob', component:FirstJobComponent}
+];
--- src/app/first-job/first-job.component.ts
+++ src/app/first-job/first-job.component.ts
@@ -1,14 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-first-job',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './first-job.component.html',
-  styleUrl: './first-job.component.sass'
+  styleUrl: './first-job.component.scss'
 })
 export class FirstJobComponent implements OnInit{
-  active: boolean = false;
+  active: boolean = true;
 
   constructor() {}
 
